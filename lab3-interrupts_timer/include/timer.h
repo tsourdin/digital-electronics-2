9159d21bce8bc52b2d5457c8960a245f26cb7d73
tsourdin <thibault.sourdin@mailo.com>
2022-10-18 09:52:43 +0200

    lab3-assignement : Forgot a function documentation

--- lab3-interrupts_timer/include/timer.h
+++ lab3-interrupts_timer/include/timer.h
@@ -86,6 +86,7 @@
  * @name  Definitions for 8-bit Timer/Counter2
  * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 8, F_CPU = 16 MHz
  */
+/** @brief Stop timer, prescaler 000 --> STOP */
 #define TIM2_stop()             TCCR2B &= ~((1<<CS22) | (1<<CS21) | (1<<CS20));
 /** @brief Set overflow 16us, prescaler 001 --> 1 */
 #define TIM2_overflow_16us()    TCCR2B &= ~((1<<CS22) | (1<<CS21)); TCCR2B |= (1<<CS20);
