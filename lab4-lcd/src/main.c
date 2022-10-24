689a6e7dafe0e281527e6759dfa6f0a17582f85c
tsourdin <thibault.sourdin@mailo.com>
2022-10-24 23:47:05 +0200

    lab4 : post-lab work
    Finished the display application, added the postlab work (flow diagram
    and schematics).

--- lab4-lcd/src/main.c
+++ lab4-lcd/src/main.c
@@ -43,11 +43,27 @@
  **********************************************************************/
 int main(void)
 {
+     // Custom character definition using https://omerk.github.io/lcdchargen/
+    uint8_t customChar[48] = {
+        // address = 0
+        0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000,
+        // address = 1
+        0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000,
+        // address = 2
+        0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000,
+        // address = 3
+        0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100,
+        // address = 4
+        0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110,
+        // address = 5
+        0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111
+    };
+
     GPIO_mode_output(&DDRB, PB2);
     GPIO_write_high(&PORTB, PB2);
 
     // Initialize LCD display
-    lcd_init(LCD_DISP_ON_BLINK);
+    lcd_init(LCD_DISP_ON);
 
     // Put string(s) on LCD screen
     lcd_gotoxy(1, 0);
@@ -62,10 +78,28 @@ int main(void)
     lcd_gotoxy(11, 1);
     lcd_putc('c');   
 
+    // Copy the custom characters in memory
+    lcd_command(1<<LCD_CGRAM);       // Set addressing to CGRAM (Character Generator RAM)
+                                     // ie to individual lines of character patterns
+    for (uint8_t i = 0; i < 48; i++)  // Copy new character patterns line by line to CGRAM
+        lcd_data(customChar[i]);
+    lcd_command(1<<LCD_DDRAM);       // Set addressing back to DDRAM (Display Data RAM)
+                                     // ie to character codes
+
     // Configuration of 8-bit Timer/Counter2 for Stopwatch update
     // Set the overflow prescaler to 16 ms and enable interrupt
     TIM2_overflow_16ms();
     TIM2_overflow_interrupt_enable();
+
+    // Configuration of 8-bit Timer/Couter0 : overflow 16ms
+    // Used for the progression bar on position b
+    TIM0_overflow_16ms();
+    TIM0_overflow_interrupt_enable();
+
+    // Configuration of 8-bit Timer/Counter1 : overflow 262ms
+    // Used for the running text on position c
+    TIM1_overflow_262ms();
+    TIM1_overflow_interrupt_enable();
 
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -105,10 +139,6 @@ ISR(TIMER2_OVF_vect)
         // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
         tenths++;
 
-        itoa(tenths, string, 10);  // Convert decimal value to string
-        lcd_gotoxy(7, 0);
-        lcd_puts(string);
-
         if(tenths > 9){
             tenths = 0;
             seconds++;
@@ -121,6 +151,7 @@ ISR(TIMER2_OVF_vect)
                     minutes = 0;
                 }
 
+                // If minutes are only on 1 character, we want to display 01, 02, 03... and not 10, 20, 30...
                 if(minutes <= 9){
                     itoa(minutes, &string[1], 10);
                     string[0]='0';
@@ -131,6 +162,7 @@ ISR(TIMER2_OVF_vect)
 
             }
             
+            // If seconds are only on 1 character, we want to display 01, 02, 03... and not 10, 20, 30...
             if(seconds <= 9 ){
                 itoa(seconds, &string[1], 10);
                 string[0]='0';
@@ -139,11 +171,69 @@ ISR(TIMER2_OVF_vect)
             lcd_gotoxy(4, 0);
             lcd_puts(string);
 
+            // Display the square value of the seconds at position 'a'   
             lcd_gotoxy(11,0);
             itoa(seconds*seconds, string, 10);
             lcd_puts(string);
         }
-        
+
+        // Display tenths
+        itoa(tenths, string, 10);  // Convert decimal value to string
+        lcd_gotoxy(7, 0);
+        lcd_puts(string);        
     }
     // Else do nothing and exit the ISR
+}
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter0 overflow interrupt
+ * Purpose:  Display and update the progression bar every 16ms
+ **********************************************************************/
+ISR(TIMER0_OVF_vect)
+{
+    static uint8_t position = 0;
+    static uint8_t symbol = 0;
+
+    lcd_gotoxy(position+1,1);
+    lcd_putc(symbol);
+    symbol ++;
+
+    if(symbol > 5){
+        symbol = 0;
+        position ++;
+    
+        // Clear the line
+        if(position > 9){
+            position = 0;
+            lcd_gotoxy(1,1);
+            for (int i=0; i<=9; i++){
+                lcd_gotoxy(i+1, 1);
+                lcd_putc(0x00);
+            }
+        }
+    }
+}
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter0 overflow interrupt
+ * Purpose:  Display and update the progression bar every 16ms
+ **********************************************************************/
+ISR(TIMER1_OVF_vect)
+{
+    char running_text[] = " I like Digital electronics!\n";
+    static uint8_t index = 0;
+    static uint8_t flag = 0b0;
+    
+    // Shift the variable to display every 500ms
+    if(flag == 0b1){
+        index++;
+        if(index > 29) index = 0;
+    }
+
+    lcd_gotoxy(11,1);
+    lcd_puts(running_text+index);
+
+    flag ^= 0b1;
 }
